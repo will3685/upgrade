68fc0d6b7cb07870b99b91f16626c761332a6a5f
Will Theard <lavardwilltheard@yahoo.com>
2022-04-10 17:38:07 -0300

    Add seed for fake data

--- Gemfile
+++ Gemfile
@@ -24,7 +24,10 @@ gem 'redis', '~> 4.0'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'faker'
 
+# I decided to use react as my frontend library, u should to
+gem 'react-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+User.destroy_all 
+
+puts "Creating users"
+(0..20).each do |f|
+  email = "lavardwilltheard@yahoo.com"
+  password = rand(100000..600000)
+  firstname = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  bio = Faker::BossaNova.song
+  turma = Faker::Company.name
+  ano_ingresso = rand(1900..2022)
+  ano_saida = rand(1990..2022)
+  user = User.create(email: email, encrypted_password: password, firstname: firstname, lastname: lastname, turma: turma, ano_ingresso: ano_ingresso, ano_saida: ano_saida)
+  p "Created #{user.firstname} user"
+end
+
